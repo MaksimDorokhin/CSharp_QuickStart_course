36c2d470475e1490df6929270ab84edcce215760
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-04 22:43:12 +0300

    Added turtle game pjt

---
+++ TurtleGame/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace TurtleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+            Turtle.PenUp();
+            GraphicsWindow.BrushColor = "Red";
+
+            var eatX = 200;
+            var eatY = 200;
+
+            var eat = Shapes.AddRectangle(10, 10);
+            Shapes.Move(eat, eatX, eatY);
+
+            while(true)
+            {
+                Turtle.Move(10);
+                if (Turtle.X == eatX && Turtle.Y == eatY)
+                {
+                    eatX = eatX + 20;
+                    Shapes.Move(eat, eatX, eatY);
+                }
+            }
+        }
+
+        private static void GraphicsWindow_KeyDown()
+        {
+            if (GraphicsWindow.LastKey == "Up")
+            {
+                Turtle.Angle = 0;
+            }
+            else if (GraphicsWindow.LastKey == "Right")
+            {
+                Turtle.Angle = 90;
+            }
+            else if (GraphicsWindow.LastKey == "Down")
+            {
+                Turtle.Angle = 180;
+            }
+            else if (GraphicsWindow.LastKey == "Left")
+            {
+                Turtle.Angle = 270;
+            }
+        }
+    }
+}
