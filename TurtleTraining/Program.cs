2b76c738bb066fff358328626666641e20d59d71
Maksim Dorokhin <max.dorokhin@gmail.com>
2022-05-04 17:19:01 +0300

    Added Tort writting using fuctions to Turtle training

--- TurtleTraining/Program.cs
+++ TurtleTraining/Program.cs
@@ -7,29 +7,87 @@ using Microsoft.SmallBasic.Library;
 
 namespace TurtleTraining
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Turtle.Speed = 9;
-            for(int i = 0; i < 4; i++)
-            {
-                Turtle.Move(50);
-                Turtle.TurnRight();
-                Turtle.Move(50);
-                Turtle.TurnRight();
-                Turtle.Move(50);
-                Turtle.TurnLeft();
-                Turtle.Move(50);
-                Turtle.TurnLeft();
-            }
-
-            /*while (i < 6)
-			{
-				Turtle.Move(100);
-				Turtle.Turn(60);
-				i++;
-			}*/
+    class Program
+    {
+        static void WriteT(int size)
+        {
+            // Letter T begin
+            Turtle.Angle = 0;
+            Turtle.Move(size);
+            Turtle.TurnLeft();
+            Turtle.Move(size - size / 2);
+            Turtle.Angle = 90;
+            Turtle.Move(size);
+            // Letter T end
         }
-	}
+        static void WriteO(int size)
+        {
+            // Letter O begin
+            Turtle.Angle = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                Turtle.Move(size);
+                Turtle.TurnRight();
+            }
+            // Letter O end
+        }
+        static void WriteP(int size)
+        {
+            // Letter P begin
+            Turtle.Angle = 0;
+            Turtle.Move(size);
+            for (int i = 0; i < 3; i++)
+            {
+                Turtle.TurnRight();
+                Turtle.Move(size / 2);
+            }
+            // Letter O end
+        }
+        static void Main(string[] args)
+		{
+            //Turtle.Speed = 9;
+            ///* for(int i = 0; i < 4; i++)
+            // {
+            //     Turtle.Move(50);
+            //     Turtle.TurnRight();
+            //     Turtle.Move(50);
+            //     Turtle.TurnRight();
+            //     Turtle.Move(50);
+            //     Turtle.TurnLeft();
+            //     Turtle.Move(50);
+            //     Turtle.TurnLeft();
+            // }*/
+
+            //while (i < 6)
+            //{
+            //    Turtle.Move(100);
+            //    Turtle.Turn(60);
+            //    i++;
+            //}
+
+            // Tort
+
+            Turtle.Speed = 8;
+            Turtle.X = 200;
+            Turtle.Y = 200;
+
+            WriteT(60);
+
+            Turtle.X = 240;
+            Turtle.Y = 200;
+
+            WriteO(30);
+
+            Turtle.X = 290;
+            Turtle.Y = 200;
+
+            WriteP(30); 
+
+            Turtle.X = 340;
+            Turtle.Y = 200;
+
+            WriteT(30);
+
+        }
+    }
 }
